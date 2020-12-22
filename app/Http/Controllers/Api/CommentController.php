9bd198e41d2895a8d2502dac7c2d1136a1b83192
vc-dhavaljoshi <dhaval.joshi@viitor.cloud>
2020-12-22 15:33:55 +0530

    Created API for store comment

---
+++ app/Http/Controllers/Api/CommentController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\ApiBaseController;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends ApiBaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $comment = new Comment();
+            $comment->user = $request->user;
+            $comment->post_id = 1;
+            $comment->parent_id = $request->parent_id ?? null;
+            $comment->content = $request->content;
+            $response = [];
+
+            if($comment->save()) {
+                $response['data'] = $comment;
+                $response['status_code'] = 200;
+                $response['message'] = 'Comment added successfully!';
+
+                return $this->successResponse($response);
+
+            } else {
+                $response['data'] = [];
+                $response['status_code'] = 500;
+                $response['message'] = 'something went to wrong!';
+
+                return $this->errorResponse($response, 500);
+            }
+
+        } catch (\Exception $exception) {
+            $response['data'] = [];
+            $response['status_code'] = 500;
+            $response['message'] = 'something went to wrong!';
+
+            return $this->errorResponse($response, 500);
+        }
+    }
+}
---
+++ app/Http/Controllers/ApiBaseController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiBaseController extends Controller
+{
+    private $response = [
+        'status_code' => null,
+        'data'  => null,
+        'message'   => null
+    ];
+    
+    /**
+     * errorResponse
+     *
+     * @param  mixed $data
+     * @param  mixed $message
+     * @param  mixed $statusCode
+     * @return void
+     */
+    public function errorResponse($response, $statusCode = 500)
+    {
+        $this->response['status_code'] = $statusCode;
+        $this->response['data'] = $response['data'];
+        $this->response['message'] = $response['message'];
+
+        return $this->response;
+    }
+    
+    /**
+     * successResponse
+     *
+     * @param  mixed $data
+     * @param  mixed $message
+     * @return void
+     */
+    public function successResponse($response)
+    {
+        $this->response['status_code'] = 200;
+        $this->response['data'] = $response['data'];
+        $this->response['message'] = $response['message'];
+
+        return $this->response;
+    }
+}
